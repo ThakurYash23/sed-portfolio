18430b3eb78bb0debf9b56eeac7010d0b5fc4470
“ThakurYash0023” <yashofficial0023@gmail.com>
2023-07-19 19:39:22 +0530

    Initializing Project. It is first commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      Hi This is Home Page of this site
+    </div>
+  )
+}
+
+export default Home
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      Hi this is Navigation component
+    </div>
+  )
+}
+
+export default Navbar
